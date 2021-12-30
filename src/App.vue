b96d939410e3349e15633d99f0fbf76d2e35eb25
LAPTOP-RI2VH723\LVO <15827969980@163.com>
2021-12-30 10:02:31 +0800

    增加子组件

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <template>
   <div>
       <h2>{{getFoo(222)}}</h2>
+      <Children/>
   </div>
 </template>
  
 <script >
-    export default {
-        data(){
-            return {
-                name:'hello'
-            }
-        },
-        computed:{
-            getFoo(){
-                return function(num){
-                    return this.name + num
-                }
+import Children from './components/Children.vue'
+export default {
+    data(){
+        return {
+            name:'hello'
+        }
+    },
+    components:{
+        Children,
+    },
+    computed:{
+        getFoo(){
+            return function(num){
+                return this.name + num
             }
         }
     }
+}
 </script>
  
  <style scoped >
---
+++ src/components/Children.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>我是:{{name}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            name:'皮卡丘'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
