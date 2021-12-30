0bb0d037c8d886647705565ccd1b7e68b1a21a02
LAPTOP-RI2VH723\LVO <15827969980@163.com>
2021-12-30 11:24:05 +0800

    addPostCss

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
           },
           {
             test: /\.css$/i,
-            use: ["style-loader", "css-loader",'postcss-loader' ],       
+            use: ['style-loader', 'css-loader','postcss-loader']
           },
           {
             test: /\.(svg|gif|png|jpe?g)/,
