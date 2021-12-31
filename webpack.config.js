148c04c8fa9d31f2f9e7cfe68660634468b29a0c
LAPTOP-RI2VH723\LVO <15827969980@163.com>
2021-12-31 09:47:33 +0800

    粒度优化update

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack')
 const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
-    entry: {index:'./index.js',polyfills: './src/polyfills',},
+    entry: {index:'./index.js'},
     output: {
         clean: true,
         path: path.resolve(__dirname, 'dist'),
